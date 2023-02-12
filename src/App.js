0a124b84dfd32792e7a1828998a889ae4ae9fbef
Rumazor <th3rum2084@gmail.com>
2023-02-12 09:22:31 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Menu from "./components/Menu/Menu";
-import mashwinRoute from "./components/gameRoutes/mashwinRoute";
+import MashwinRoute from "./components/gameRoutes/MashwinRoute";
 import Loading from "./components/Loading/Loading";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
       <>
         <Routes>
           <Route index path="/" element={<Menu />} />
-          <Route path="/mashwinRoute" element={<mashwinRoute />} />
+          <Route path="/mashwinRoute" element={<MashwinRoute />} />
           <Route path="/loading" element={<Loading />} />
         </Routes>
       </>
